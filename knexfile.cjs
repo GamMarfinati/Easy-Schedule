7c7c6253e782a5201e919b173ade9d9008975aca
GamMarfinati <jonathan.marfinati@gmail.com>
2025-12-13 18:16:46 -0300

    DEBUG: Add DATABASE_URL logging to diagnose connection issue

--- knexfile.cjs
+++ knexfile.cjs
@@ -1,5 +1,9 @@
 require('dotenv').config();
 
+// Debug: verificar se DATABASE_URL está disponível
+console.log('[DB Config] DATABASE_URL exists:', !!process.env.DATABASE_URL);
+console.log('[DB Config] DATABASE_URL host:', process.env.DATABASE_URL ? new URL(process.env.DATABASE_URL).host : 'NOT SET');
+
 module.exports = {
   development: {
     client: "pg",
